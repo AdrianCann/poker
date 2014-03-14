d485420346ad70d89795e1b5b885f33ea70883d8
Krishna&Adrian <krishna-adrian@appacademy.io>
2014-03-14 18:55:39 -0400

    Hand class correctly evaluates a hand and compares its value with that of another hand

--- Poker/lib/hand.rb
+++ Poker/lib/hand.rb
@@ -31,24 +31,159 @@ class Hand
 
 
   def hand_value
-
+    HAND_TYPES[evaluate_hand[0]]
   end # => :trips, :flush
 
 
-  def compare(other_hand)
-    own_value = self.evaluate_hand
+  def beats(other_hand)
+    own_value = evaluate_hand
     opp_value = other_hand.evaluate_hand
 
+    own_value.each_index do |i|
+      return 1  if own_value[i] > opp_value[i]
+      return -1 if opp_value[i] > own_value[i]
+    end
 
+    0
     # compare the indices of the two value arrays
-    # first time one array-value is greater than the other, that hand is greater
+  # first time one array-value is greater than the other, that hand is greater
   end # => -1, 0, or 1
 
-  private
+  protected
   def evaluate_hand
-
+    value_arr = []
+    rank_counts = freq_hash
+    if rank_counts.keys.count == 5
+      eval_no_pair_hand(rank_counts) #
+    else
+      eval_pair_hand(rank_counts)
+    end
   end# => array
 
+  private
+  def freq_hash
+    rank_counts = Hash.new(0)
+    cards.each do |card|
+      rank_counts[card.val] += 1
+    end
+    rank_counts
+  end
+
+  def eval_no_pair_hand(rank_counts)
+
+    is_flush = is_flush?
+    is_straight = is_straight?(rank_counts)
+
+    return assign_str_fls(rank_counts) if is_flush && is_straight
+    return assign_fls(rank_counts) if is_flush
+    return assign_str(rank_counts) if is_straight
+
+    assign_high_card(rank_counts)
+  end
+
+
+  def is_flush?
+    suit_to_check = cards.first.suit
+    cards.all? { |card| card.suit == suit_to_check }
+  end
+
+  def is_straight?(rank_counts)
+    sorted_ranks = rank_counts.keys.sort
+    # Check if there is an Ace; if so there may be a straight: A-2-3-4-5
+    if sorted_ranks.include? 14
+      return true if [14, 2, 3, 4, 5].all? { |rank| sorted_ranks.include?(rank) }
+    end
+
+    is_straight = true
+    sorted_ranks.each_index do |i|
+      next if (i + 1) == sorted_ranks.length
+      is_straight &&= (sorted_ranks[i + 1] - sorted_ranks[i] == 1)
+    end
+    is_straight
+  end
+
+  def assign_str_fls(rank_counts)
+    [8, rank_counts.keys.min]
+  end
+
+  def assign_fls(rank_counts)
+    [5] + rank_counts.keys.sort.reverse
+  end
+
+  def assign_str(rank_counts)
+    [4, rank_counts.keys.min]
+  end
+
+  def assign_high_card(rank_counts)
+    [0] + rank_counts.keys.sort.reverse
+  end
+
+
+  def eval_pair_hand(rank_counts)
+    max_freq = rank_counts.values.max
+
+    if max_freq == 2
+      eval_pairs(rank_counts)
+    elsif max_freq == 3
+      eval_trips(rank_counts)
+    else
+      assign_four_kind(rank_counts)
+    end
+  end
+
+  def eval_pairs(rank_counts)
+    if rank_counts.keys.count == 4
+      assign_pr(rank_counts)
+    else
+      assign_two_pr(rank_counts)
+    end
+  end
+
+  def assign_pr(rank_counts)
+    sorted_ranks = rank_counts.keys.sort.reverse
+    pair_rank = rank_counts.detect { |rank, count| count == 2}.first
+    sorted_ranks.delete(pair_rank)
+
+    [1, pair_rank] + sorted_ranks
+  end
+
+  def assign_two_pr(rank_counts)
+
+    sorted_ranks = rank_counts.keys.sort.reverse
+    unpair_rank = rank_counts.detect { |rank, count| count == 1 }.first
+    sorted_ranks.delete(unpair_rank)
+
+    [2] + sorted_ranks + [unpair_rank]
+  end
+
+  def eval_trips(rank_counts)
+    if rank_counts.keys.count == 2
+      assign_full_hs(rank_counts)
+    else
+      assign_three_kd(rank_counts)
+    end
+  end
+
+  def assign_full_hs(rank_counts)
+    sorted_ranks = rank_counts.keys
+    sorted_ranks.reverse! if (rank_counts[sorted_ranks[0]] == 2)
+    [6] + sorted_ranks
+  end
+
+  def assign_three_kd(rank_counts)
+    sorted_ranks = rank_counts.keys.sort.reverse
+    three_rank = rank_counts.detect { |rank, count| count == 3}.first
+    sorted_ranks.delete(three_rank)
+
+    [3, three_rank] + sorted_ranks
+  end
+
+  def assign_four_kind(rank_counts)
+    sorted_ranks = rank_counts.keys
+    sorted_ranks.reverse! if (rank_counts[sorted_ranks[0]] == 1)
+
+    [7] + sorted_ranks
+  end
 
 
 end
--- Poker/spec/hand_spec.rb
+++ Poker/spec/hand_spec.rb
@@ -3,9 +3,12 @@ require 'deck'
 
 describe Hand do
 
+  # let(:cards)
+  # subject(:hand) { hand  = Hand.new(cards) }
+
   describe "#initialize" do
     let!(:deck) { deck = Deck.new }
-    let!(:cards) { cards = deck.deal(5)}
+    let!(:cards) { cards = deck.deal(5) }
     subject(:hand) { hand  = Hand.new(cards) }
 
 
@@ -39,11 +42,12 @@ describe Hand do
       Card.new(:clubs, :queen),
       Card.new(:diamonds, :deuce)]
     }
+
     subject(:hand) { hand  = Hand.new(cards) }
 
     it "removes the proper cards from its own card array" do
       discard_cards = [Card.new(:clubs, :ace),
-      Card.new(:clubs, :queen)]
+        Card.new(:clubs, :queen)]
 
       proper_remain_cards = [Card.new(:clubs, :deuce),
       Card.new(:hearts, :deuce),
@@ -56,68 +60,94 @@ describe Hand do
 
   end
 
-  describe "#hand_value" do
+  describe "evaluating and comparing hands" do
 
-    context "correctly identifies each of the nine types of hand" do
-
-      let! (:cA) { cA = Card.new(:clubs, :ace) }
-      let! (:cK) { cK = Card.new(:clubs, :king) }
-      let! (:cQ) { cQ = Card.new(:clubs, :queen) }
-      let! (:cJ) { cJ = Card.new(:clubs, :jack) }
-      let! (:cT) { cT = Card.new(:clubs, :ten) }
-      let! (:c5) { c5 = Card.new(:clubs, :five) }
-      let! (:dQ) { dQ = Card.new(:diamonds, :queen) }
-      let! (:h5) { h5 = Card.new(:hearts, :five) }
-      let! (:s5) { s5 = Card.new(:spades, :five) }
-      let! (:d5) { d5 = Card.new(:diamonds, :five) }
+    let! (:cA) { cA = Card.new(:clubs, :ace) } #
+    let! (:cK) { cK = Card.new(:clubs, :king) } #
+    let! (:cQ) { cQ = Card.new(:clubs, :queen) }
+    let! (:cJ) { cJ = Card.new(:clubs, :jack) }
+    let! (:cT) { cT = Card.new(:clubs, :ten) } #
+    let! (:c5) { c5 = Card.new(:clubs, :five) }
+    let! (:dQ) { dQ = Card.new(:diamonds, :queen) } #
+    let! (:h5) { h5 = Card.new(:hearts, :five) }
+    let! (:s5) { s5 = Card.new(:spades, :five) } #
+    let! (:d5) { d5 = Card.new(:diamonds, :five) }
 
 
-      let! (:no_hand) { no_hand  = Hand.new([cA, cK, cT, dQ, s5]) }
-      let! (:one_pr) { one_pr  = Hand.new([cA, cT, cQ, dQ, d5]) }
-      let! (:two_pr) { two_pr  = Hand.new([d5, s5, cQ, cK, dQ]) }
-      let! (:trips) { trips  = Hand.new([d5, dQ, c5, cA, s5]) }
-      let! (:strt) { strt  = Hand.new([cT, cA, dQ, cK, cJ]) }
-      let! (:fls) { fls  = Hand.new([c5, cK, cQ, cA, cJ]) }
-      let! (:boat) { boat  = Hand.new([cQ, s5, d5, c5, dQ]) }
-      let! (:quads) { quads  = Hand.new([d5, s5, cQ, c5, h5]) }
-      let! (:str_fls) { str_fls  = Hand.new([cT, cA, cQ, cK, cJ]) }
+    let! (:no_hand) { no_hand  = Hand.new([cA, cK, cT, dQ, s5]) }
+    let! (:one_pr) { one_pr  = Hand.new([cA, cT, cQ, dQ, d5]) }
+    let! (:two_pr) { two_pr  = Hand.new([d5, s5, cQ, cK, dQ]) }
+    let! (:trips) { trips  = Hand.new([d5, cQ, c5, cJ, h5]) }
+    let! (:strt) { strt  = Hand.new([cT, cA, dQ, cK, cJ]) }
+    let! (:fls) { fls  = Hand.new([c5, cK, cQ, cA, cJ]) }
+    let! (:boat) { boat  = Hand.new([cQ, s5, d5, c5, dQ]) }
+    let! (:quads) { quads  = Hand.new([d5, s5, cQ, c5, h5]) }
+    let! (:str_fls) { str_fls  = Hand.new([cT, cA, cQ, cK, cJ]) }
 
 
+    describe "#hand_value" do
 
-      it "recognizes no_pair"  do
-        expect(no_hand.hand_value).to eq(:no_pair)
+      context "correctly identifies each of the nine types of hand" do
+
+        it "recognizes no_pair"  do
+          expect(no_hand.hand_value).to eq(:no_pair)
+        end
+
+        it "recognizes pair" do
+          expect(one_pr.hand_value).to eq(:pair)
+        end
+
+        it "recognizes two_pair" do
+          expect(two_pr.hand_value).to eq(:two_pair)
+        end
+
+        it "recognizes three_of_a_kind" do
+          expect(trips.hand_value).to eq(:three_of_a_kind)
+        end
+
+        it "recognizes straight" do
+          expect(strt.hand_value).to eq(:straight)
+        end
+
+        it "recognizes flush" do
+          expect(fls.hand_value).to eq(:flush)
+        end
+
+        it "recognizes full_house" do
+          expect(boat.hand_value).to eq(:full_house)
+        end
+
+        it "recognizes four_of_a_kind" do
+          expect(quads.hand_value).to eq(:four_of_a_kind)
+        end
+
+        it "recognizes straight_flush" do
+          expect(str_fls.hand_value).to eq(:straight_flush)
+        end
+
       end
 
-      it "recognizes pair" do
-        expect(one_pr.hand_value).to eq(:pair)
+    end
+
+    describe "#beats" do
+
+      it "correctly determines the better of two different types of hand" do
+        expect(no_hand.beats(trips)).to eq(-1)
       end
 
-      it "recognizes two_pair" do
-        expect(two_pr.hand_value).to eq(:two_pair)
+      it "correctly breaks ties between by looking at highest cards" do
+        other_fls = double("other_fls")
+        other_fls.should_receive(:evaluate_hand).and_return([5, 14, 13, 12, 8, 5])
+
+        expect(fls.beats(other_fls)).to eq(1)
       end
 
-      it "recognizes three_of_a_kind" do
-        expect(trips.hand_value).to eq(:three_of_a_kind)
-      end
+      it "indicates a tie when there is one" do
+        other_fls = double("other_fls")
+        other_fls.should_receive(:evaluate_hand).and_return([5, 14, 13, 12, 11, 5])
 
-      it "recognizes straight" do
-        expect(strt.hand_value).to eq(:straight)
-      end
+        expect(fls.beats(other_fls)).to eq(0)
 
-      it "recognizes flush" do
-        expect(fls.hand_value).to eq(:flush)
-      end
-
-      it "recognizes full_house" do
-        expect(boat.hand_value).to eq(:full_house)
-      end
-
-      it "recognizes four_of_a_kind" do
-        expect(quads.hand_value).to eq(:four_of_a_kind)
-      end
-
-      it "recognizes straight_flush" do
-        expect(str_fls.hand_value).to eq(:straight_flush)
       end
 
     end
